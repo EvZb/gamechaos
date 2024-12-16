3eac8e4b7afdbecb6323aac16eb40ede8be55251
EvZb <130612260+EvZb@users.noreply.github.com>
2024-12-16 14:47:56 -0800

    organize sprites

--- scripts/init/variables.gd
+++ scripts/init/variables.gd
@@ -8,3 +8,14 @@ var LOGINDEX:int = 0
 const BIOMETILES:Array[Vector2i] = [Vector2i(0,0),Vector2i(1,0),Vector2i(2,0),Vector2i(3,0),Vector2i(4,0),Vector2i(5,0),Vector2i(6,0),Vector2i(7,0),Vector2i(8,0),Vector2i(9,0)]
 const BIOMENAMES:PackedStringArray = ["Red","Orange","Yellow","Green","Teal","Blue","Pink","White","Grey","Black"]
 const LAYERPATH:String = "/root/Main/Game/World/Layers/"
+const TEXTURES:Dictionary = {
+	"Biome":null,
+	"Area":null,
+	"Ground":null,
+	"Floor":null,
+	"Wall":null,
+	"Object":null,
+	"Item":null,
+	"Character":null,
+	"Creature":null
+}
